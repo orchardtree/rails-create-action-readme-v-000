9820906b4566c82978312cf5034e900ef9c6eb3d
Jesse Wenger <wengerj@gmail.com>
2021-04-26 16:32:51 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,5 +13,9 @@ class PostsController < ApplicationController
 
   # add create method here
   def create
+    post = Post.new
+    post.title = params[:title]
+    post.description = params[:description]
+    post.save
   end
 end
